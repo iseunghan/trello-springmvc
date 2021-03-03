c286b766e120d992ae337c6c9b3541cb8374d6e6
iseunghan <tmdgks301@naver.com>
2021-03-03 10:23:35 +0900

    first commit

---
+++ src/main/java/me/iseunghan/trellospringmvc/TrelloSpringmvcApplication.java
@@ -0,0 +1,13 @@
+package me.iseunghan.trellospringmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrelloSpringmvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrelloSpringmvcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/iseunghan/trellospringmvc/TrelloSpringmvcApplicationTests.java
@@ -0,0 +1,13 @@
+package me.iseunghan.trellospringmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrelloSpringmvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
