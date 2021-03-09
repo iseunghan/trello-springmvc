bdd08a5e8bf93ab63ec11eb0da1ef145e55207eb
iseunghan <tmdgks301@naver.com>
2021-03-09 22:43:37 +0900

    refactor: cards 조회 시 pocketId를 이용해 조회하도록 변경
    - CardRepository.findCardsByPocket_Id: cards 조회 시 pocketId를 이용해 조회하도록 변경

--- src/main/java/me/iseunghan/trellospringmvc/repository/CardRepository.java
+++ src/main/java/me/iseunghan/trellospringmvc/repository/CardRepository.java
@@ -4,7 +4,9 @@ import me.iseunghan.trellospringmvc.domain.Card;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CardRepository extends JpaRepository<Card, Long> {
-
+    List<Card> findCardsByPocket_Id(Long pocketId);
 }
