1daf073651871bca339ab88237b9a87b33e1d335
iseunghan <tmdgks301@naver.com>
2021-03-18 17:41:44 +0900

    refactor: BoardDto 수정
    - BoardDto.java: 무한루프 방지를 위해, Pocket 대신 PocketDto 저장

--- src/main/java/me/iseunghan/trellospringmvc/domain/BoardDto.java
+++ src/main/java/me/iseunghan/trellospringmvc/domain/BoardDto.java
@@ -1,10 +1,54 @@
 package me.iseunghan.trellospringmvc.domain;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
 public class BoardDto {
 
+    private Long boardId;
     private String title;
     private int position;
     private BoardColor boardColor;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    private List<PocketDto> pockets = new ArrayList<>();
+
+    public void setBoardColor(BoardColor boardColor) {
+        this.boardColor = boardColor;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public Long getBoardId() {
+        return boardId;
+    }
+
+    public void setBoardId(Long boardId) {
+        this.boardId = boardId;
+    }
+
+    public List<PocketDto> getPockets() {
+        return pockets;
+    }
+
+    public void setPockets(List<PocketDto> pockets) {
+        this.pockets = pockets;
+    }
 
     public String getTitle() {
         return title;
