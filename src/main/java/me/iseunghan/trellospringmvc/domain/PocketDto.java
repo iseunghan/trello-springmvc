a6f606dc7173f0e12414ef5d2227fe816b0a44f5
iseunghan <tmdgks301@naver.com>
2021-03-18 17:47:17 +0900

    refactor: PocketDto 수정
    - PocketDto.java: 무한루프 방지를 위해 아래 두 가지 적용
             - Board 대신 boardId 저장
             - Card 대신 CardDto 컬렉션으로 저장

--- src/main/java/me/iseunghan/trellospringmvc/domain/PocketDto.java
+++ src/main/java/me/iseunghan/trellospringmvc/domain/PocketDto.java
@@ -1,10 +1,17 @@
 package me.iseunghan.trellospringmvc.domain;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
 public class PocketDto {
+    private Long pocketId;
     private String title;
     private int position;
     private Long boardId;
-    private Long cardId;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    private List<CardDto> cards = new ArrayList<>();
 
     public String getTitle() {
         return this.title;
@@ -30,11 +37,35 @@ public class PocketDto {
         this.boardId = boardId;
     }
 
-    public Long getCardId() {
-        return this.cardId;
+    public List<CardDto> getCards() {
+        return cards;
     }
 
-    public void setCardId(Long cardId) {
-        this.cardId = cardId;
+    public void setCards(List<CardDto> cards) {
+        this.cards = cards;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public Long getPocketId() {
+        return pocketId;
+    }
+
+    public void setPocketId(Long pocketId) {
+        this.pocketId = pocketId;
     }
 }
