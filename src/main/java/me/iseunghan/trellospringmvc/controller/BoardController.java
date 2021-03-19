c31fb2082e7861c93777aee79cd72b2698949a4c
iseunghan <tmdgks301@naver.com>
2021-03-19 09:06:49 +0900

    feat: BoardController REST API 개발
    - BoardController.java: BoardDto를 BoardResource로 한번 더 감싸서 응답

--- src/main/java/me/iseunghan/trellospringmvc/controller/BoardController.java
+++ src/main/java/me/iseunghan/trellospringmvc/controller/BoardController.java
@@ -6,12 +6,12 @@ import me.iseunghan.trellospringmvc.domain.BoardDto;
 import me.iseunghan.trellospringmvc.domain.BoardResource;
 import me.iseunghan.trellospringmvc.service.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.CollectionModel;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
-import java.util.ArrayList;
 import java.util.List;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
@@ -22,27 +22,21 @@ public class BoardController {
 
     @Autowired
     private BoardService boardService;
+    @Autowired
+    private BoardResource boardResource;
 
     @GetMapping
     public ResponseEntity list() {
         List<Board> boards = boardService.findAll();
-        List<BoardResource> resources = new ArrayList<>();
-
-        BoardResource boardResource = null;
-        for (Board board : boards) {
-            boardResource = new BoardResource(board);
-            resources.add(boardResource);
-        }
-
-        return ResponseEntity.ok(resources);
+        CollectionModel<BoardResource> boardResources = boardResource.toCollectionModel(boards);
+        return ResponseEntity.ok(boardResources);
     }
 
     @GetMapping(value = "/{boardId}")
     public ResponseEntity show(@PathVariable Long boardId) throws NotFoundException {
         Board board = boardService.findOne(boardId);
-        BoardResource boardResource = new BoardResource(board);
-
-        return ResponseEntity.ok(boardResource);
+        BoardResource resource = boardResource.toModel(board);
+        return ResponseEntity.ok(resource);
     }
 
     @PostMapping
@@ -50,24 +44,22 @@ public class BoardController {
         System.out.println(boardDto.getBoardColor() + ", " + boardDto.getTitle());
         Long id = boardService.addBoard(boardDto);
         Board board = boardService.findOne(id);
-        BoardResource boardResource = new BoardResource(board);
+        BoardResource resource = boardResource.toModel(board);
         URI uri = linkTo(BoardController.class).slash(board.getId()).withSelfRel().toUri();
-
-        return ResponseEntity.created(uri).body(boardResource);
+        return ResponseEntity.created(uri).body(resource);
     }
 
     @PatchMapping(value = "/{boardId}")
-    public ResponseEntity update(@PathVariable Long boardId, BoardDto boardDto) throws NotFoundException {
+    public ResponseEntity update(@PathVariable Long boardId, @RequestBody BoardDto boardDto) throws NotFoundException {
+        System.out.println(boardDto.getTitle() + " , " + boardDto.getBoardColor());
         Board board = boardService.updateBoard(boardId, boardDto);
-        BoardResource boardResource = new BoardResource(board);
-
-        return ResponseEntity.ok(boardResource);
+        BoardResource resource = boardResource.toModel(board);
+        return ResponseEntity.ok(resource);
     }
 
     @DeleteMapping(value = "/{boardId}")
     public ResponseEntity delete(@PathVariable Long boardId) throws NotFoundException {
         boolean result = boardService.deleteBoard(boardId);
-
         return ResponseEntity.ok(result);
     }
 }
