4a2778572baaf8e577731fd0fa5e28ddbd9c8113
iseunghan <tmdgks301@naver.com>
2021-03-19 09:09:18 +0900

    feat: PocketController REST API 개발
    - PocketController.java: 컨트롤러 REST API 개발

---
+++ src/main/java/me/iseunghan/trellospringmvc/controller/PocketController.java
@@ -0,0 +1,64 @@
+package me.iseunghan.trellospringmvc.controller;
+
+import javassist.NotFoundException;
+import me.iseunghan.trellospringmvc.domain.Pocket;
+import me.iseunghan.trellospringmvc.domain.PocketDto;
+import me.iseunghan.trellospringmvc.domain.PocketResource;
+import me.iseunghan.trellospringmvc.service.PocketService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.util.List;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+
+@RestController
+@RequestMapping(value = "/boards/{boardId}/pockets", produces = MediaType.APPLICATION_JSON_VALUE)
+public class PocketController {
+
+    @Autowired
+    private PocketService pocketService;
+    @Autowired
+    private PocketResource pocketResource;
+
+    @GetMapping
+    public ResponseEntity list(@PathVariable Long boardId) {
+        List<Pocket> pockets = pocketService.findAll(boardId);
+        CollectionModel<PocketResource> pocketResources = pocketResource.toCollectionModel(pockets);
+        return ResponseEntity.ok(pocketResources);
+    }
+
+    @GetMapping(value = "/{pocketId}")
+    public ResponseEntity findOne(@PathVariable Long boardId, @PathVariable Long pocketId) throws NotFoundException {
+        Pocket pocket = pocketService.findOne(pocketId);
+        PocketResource resource = pocketResource.toModel(pocket);
+        return ResponseEntity.ok(resource);
+    }
+
+    @PostMapping
+    public ResponseEntity addPocket(@PathVariable Long boardId, @RequestBody PocketDto pocketDto) throws NotFoundException {
+        System.out.println("boardId :  " + boardId);
+        Long id = pocketService.addPocket(boardId, pocketDto);
+        Pocket pocket = pocketService.findOne(id);
+        PocketResource resource = pocketResource.toModel(pocket);
+        URI uri = linkTo(PocketController.class, boardId).slash(pocket.getId()).withSelfRel().toUri();
+        return ResponseEntity.created(uri).body(resource);
+    }
+
+    @PatchMapping("/{pocketId}")
+    public ResponseEntity update(@PathVariable Long boardId, @PathVariable Long pocketId, @RequestBody PocketDto pocketDto) throws NotFoundException {
+        Pocket pocket = pocketService.updatePocket(pocketId, pocketDto);
+        PocketResource resource = pocketResource.toModel(pocket);
+        return ResponseEntity.ok(resource);
+    }
+
+    @DeleteMapping("{pocketId}")
+    public ResponseEntity delete(@PathVariable Long boardId, @PathVariable Long pocketId) throws NotFoundException {
+        boolean result = pocketService.deletePocket(boardId, pocketId);
+        return ResponseEntity.ok(result);
+    }
+}
