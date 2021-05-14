06e2035c0fbc2cd684156df0a34019c1158b7e8d
iseunghan <tmdgks301@naver.com>
2021-05-15 00:05:46 +0900

    feat: 인증된 사용자 정보를 view로 전달
    - HomeController.java:
    - 인증된 유저를 session에서 꺼내서 view에 데이터를 전달해줌
    - spring security 기본 로그인 페이지 말고 커스텀 로그인 페이지로 이동하도록 처리

--- src/main/java/me/iseunghan/trellospringmvc/controller/HomeController.java
+++ src/main/java/me/iseunghan/trellospringmvc/controller/HomeController.java
@@ -2,6 +2,9 @@ package me.iseunghan.trellospringmvc.controller;
 
 import javassist.NotFoundException;
 import me.iseunghan.trellospringmvc.domain.Board;
+import me.iseunghan.trellospringmvc.domain.User;
+import me.iseunghan.trellospringmvc.repository.UserRepository;
+import me.iseunghan.trellospringmvc.security.SessionUser;
 import me.iseunghan.trellospringmvc.service.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -9,14 +12,28 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @Controller
 public class HomeController {
 
     @Autowired
+    private HttpSession httpSession;
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
     private BoardService boardService;
 
     @GetMapping(value = "/")
-    public String home() {
+    public String home(SessionUser sessionUser, Model model) {
+        sessionUser = (SessionUser) httpSession.getAttribute("user");
+        User user = userRepository.findByEmail(sessionUser.getEmail()).get();
+
+        model.addAttribute("userid", user.getId());
+        model.addAttribute("name", user.getName());
+        model.addAttribute("email", user.getEmail());
+        model.addAttribute("image", user.getImage());
+
         return "/trello/index.html";
     }
 
@@ -27,5 +44,11 @@ public class HomeController {
         model.addAttribute("boardId", board.getId());
         model.addAttribute("boardColor", board.getBoardColor());
         return "/trello/boardDetail.html";
+    }
+
+    @GetMapping(value = "/custom-login")
+    public String customLogin() {
+
+        return "/trello/custom-login.html";
     }
 }
